f3ff3860969c5b2ccdf6f4a9ed90511ebb541f57
codermanu1103 <manthanhl2003@gmail.com>
2025-12-30 00:38:00 +0530

    added some more option to dropdown menu

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -19,6 +19,12 @@ function Product(props) {
                     <option value="2">2</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
                 </select>
             </div>
             <button id="addtocart" onClick={()=>{props.updateCartCount(quantity)}}>Add to Cart</button>
