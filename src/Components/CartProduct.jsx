de4abd9535bacc77ce652241d40111e7c8ada089
codermanu1103 <manthanhl2003@gmail.com>
2026-01-04 22:56:24 +0530

    add some css to cartproduct

--- src/Components/CartProduct.jsx
+++ src/Components/CartProduct.jsx
@@ -1,5 +1,5 @@
 import productList from "../productList.json"
-
+import "./ComponentsCSS/CartProduct.css"
 function Cartproduct(props) {
     console.log(props);
     console.log(productList[0]);
@@ -14,12 +14,31 @@ function Cartproduct(props) {
         console.log("Delete");
     }
     return (
-        <>
-            <div>{props.productId}</div>
-            <button onClick={add}>+</button>
-            <span>{props.quantity}</span>
-            <button onClick={Delete}>-</button>
-        </>
+        <div id="CheckoutItem">
+            <div id="deliveryDate">Tuesday, February 05</div>
+            <div className="itemDetails">
+                <div className="productDetails">
+                    <div className="product">
+                        <div className="productImage"> <img className="productImage" src={productList[props.productId-1].image} alt="product image" /></div>
+                        <div id="productdetails">
+                            <div id="name">{productList[props.productId-1].name}</div>
+                            <div id="price">{productList[props.productId-1].price}</div>
+                        </div>
+                    </div>
+                    <div className="quantity">
+                        <span id="quantityText">Quantity: </span>
+                        <button id="addButton" className="addButton" onClick={add}> + </button>
+                        <span id="quantity"> {props.quantity} </span>
+                        <button id="deleteButton" className="addButton" onClick={Delete}> - </button>
+                    </div>
+                </div>
+                <div className="deliveryOptions">
+
+                </div>
+            </div>
+            
+            
+        </div>
     );
 }
 
